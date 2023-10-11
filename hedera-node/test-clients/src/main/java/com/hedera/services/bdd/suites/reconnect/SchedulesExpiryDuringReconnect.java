00dc12439de821db579354973b8e931ae2191694
Jeffrey Tang <jeffrey@swirldslabs.com>
2023-10-11 17:11:06 -0500

    save

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/reconnect/SchedulesExpiryDuringReconnect.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/reconnect/SchedulesExpiryDuringReconnect.java
@@ -152,7 +152,7 @@ public class SchedulesExpiryDuringReconnect extends HapiSuite {
                                 .adminKey(DEFAULT_PAYER)
                                 .logging()
                                 .advertisingCreation(),
-                        sleepFor(Duration.ofSeconds(120).toMillis()),
+                        sleepFor(Duration.ofSeconds(90).toMillis()),
                         getScheduleInfo(longLastingSchedule)
                                 .setNode(reconnectingNode)
                                 .hasScheduledTxnIdSavedBy(longLastingSchedule)
